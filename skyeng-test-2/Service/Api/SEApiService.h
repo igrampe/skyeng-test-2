28294b8d08af116872bfaa520d6f3f1514862b53
Semyon Belokovsky <igrampe@gmail.com>
2016-12-14 11:39:22 +0500

    api service

--- skyeng-test-2/Service/Api/SEApiService.h
+++ skyeng-test-2/Service/Api/SEApiService.h
@@ -8,6 +8,14 @@
 
 #import <Foundation/Foundation.h>
 
+typedef void(^SEApiServiceRequestCodeHandler)(NSError *error);
+typedef void(^SEApiServiceSignInHandler)(NSError *error, NSString *token);
+
 @protocol SEApiService <NSObject>
+
+- (NSURLSessionTask *)requestCodeForPhone:(NSString *)phone handler:(SEApiServiceRequestCodeHandler)handler;
+- (NSURLSessionTask *)requestCodeForEmail:(NSString *)email handler:(SEApiServiceRequestCodeHandler)handler;
+- (NSURLSessionTask *)signInWithEmail:(NSString *)email password:(NSString *)password handler:(SEApiServiceSignInHandler)handler;
+- (NSURLSessionTask *)signInWithEmail:(NSString *)email code:(NSString *)code handler:(SEApiServiceSignInHandler)handler;
 
 @end
--- skyeng-test-2/Service/Api/SEApiServiceBase.m
+++ skyeng-test-2/Service/Api/SEApiServiceBase.m
@@ -10,4 +10,26 @@
 
 @implementation SEApiServiceBase
 
+- (NSURLSessionTask *)requestCodeForPhone:(NSString *)phone
+                                  handler:(SEApiServiceRequestCodeHandler)handler {
+    return nil;
+}
+
+- (NSURLSessionTask *)requestCodeForEmail:(NSString *)email
+                                  handler:(SEApiServiceRequestCodeHandler)handler {
+    return nil;
+}
+
+- (NSURLSessionTask *)signInWithEmail:(NSString *)email
+                             password:(NSString *)password
+                              handler:(SEApiServiceSignInHandler)handler {
+    return nil;
+}
+
+- (NSURLSessionTask *)signInWithEmail:(NSString *)email
+                                 code:(NSString *)code
+                              handler:(SEApiServiceSignInHandler)handler {
+    return nil;
+}
+
 @end
