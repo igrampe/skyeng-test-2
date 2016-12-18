d40f6fdb5b8f956cfdb93cc50591d66c0d9b3fc4
Semyon Belokovsky <igrampe@gmail.com>
2016-12-18 22:48:31 +0500

    api emulator

--- skyeng-test-2/Service/Api/SEApiServiceEmulator.m
+++ skyeng-test-2/Service/Api/SEApiServiceEmulator.m
@@ -12,7 +12,9 @@
 
 @interface SEApiServiceEmulator ()
 
-@property (nonatomic, assign) NSInteger reqestCodeAttemps; // Для тестирование кейсов с ошибками
+@property (nonatomic, assign) NSInteger reqestCodeAttemps; // Для тестирование кейсов запроса кода с ошибками
+@property (nonatomic, assign) NSInteger passwordSignInAttemps; // Для тестирование кейсов авторизации по паролю с ошибками
+@property (nonatomic, assign) NSInteger codeSignInAttemps; // Для тестирование кейсов авторизации по коду с ошибками
 
 @end
 
@@ -50,18 +52,38 @@
 - (NSURLSessionTask *)signInWithEmail:(NSString *)email
                              password:(NSString *)password
                               handler:(SEApiServiceSignInHandler)handler {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        handler(nil, @"11");
-    });
+    if (self.passwordSignInAttemps == 0) {
+        NSError *error = [[NSError alloc] initWithDomain:SEErrorDomainApp
+                                                    code:SEErrorCodeIncorrectEmailOrPassword
+                                                userInfo:nil];
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            handler(error, nil);
+        });
+    } else {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            handler(nil, @"11");
+        });
+    }
+    self.passwordSignInAttemps++;
     return nil;
 }
 
 - (NSURLSessionTask *)signInWithEmail:(NSString *)email
                                  code:(NSString *)code
                               handler:(SEApiServiceSignInHandler)handler {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        handler(nil, @"11");
-    });
+    if (self.codeSignInAttemps == 0) {
+        NSError *error = [[NSError alloc] initWithDomain:SEErrorDomainApp
+                                                    code:SEErrorCodeIncorrectAuthCode
+                                                userInfo:nil];
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            handler(error, nil);
+        });
+    } else {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            handler(nil, @"11");
+        });
+    }
+    self.passwordSignInAttemps++;
     return nil;
 }
 
